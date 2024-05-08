68999e2f7f76552a398ea93b4727df1cb93e2981
Thomas Indelli <tindelli@uchicago.edu>
2024-05-08 15:39:47 -0500

    Group names must be provided as an array, not a comma-separated list.

--- manifests/init.pp
+++ manifests/init.pp
@@ -375,7 +375,7 @@ class archvsync(
         password_max_age => '99999',
         password_min_age => '0',
         shell            => '/bin/bash',
-        groups           => ["${user},ftp"],
+        groups           => [$user,'ftp'],
         require          => Group['ftp'],
       }
       -> file { $push_homedir:
